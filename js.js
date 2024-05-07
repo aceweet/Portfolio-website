5a7ddebde17b9f43ab9f40dc03b37f7bcb6b8224
aceweet <andresweetman@gmail.com>
2024-05-07 07:53:01 +0200

    About me section

--- js.js
+++ js.js
@@ -14,3 +14,39 @@ textElements.forEach((text) => {
     },
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        startCounting(entry.target);
+      }
+    });
+  });
+
+  var counters = document.querySelectorAll(".count-number");
+
+  counters.forEach(function (counter) {
+    observer.observe(counter);
+  });
+
+  function startCounting(element) {
+    var startValue = parseInt(element.dataset.start, 10); // Retrieve the starting value from the data-start attribute
+    var endValue = parseInt(element.dataset.target, 10);
+    var duration = 1000;
+
+    var range = endValue - startValue;
+    var current = startValue;
+    var increment = endValue > startValue ? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+
+    var timer = setInterval(function () {
+      current += increment;
+      element.textContent = current;
+
+      if (current === endValue) {
+        clearInterval(timer);
+      }
+    }, stepTime);
+  }
+});
